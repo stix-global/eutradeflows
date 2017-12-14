299f5525778e89de3c5eddb1a465105ba9ee7409
Paul Rougieux <paul.rougieux@gmail.com>
2017-12-14 15:52:37 +0100

    add country and time range selection

--- docs/visualization/shinycsv/server.R
+++ docs/visualization/shinycsv/server.R
@@ -1,35 +1,82 @@
 library(dplyr)
 library(eutradeflows)
-con <- RMySQL::dbConnect(RMySQL::MySQL(), dbname = "tradeflows")
+
+# Hack to disconnect open connections
+# RMySQL::dbDisconnect(RMySQL::dbListConnections(RMySQL::MySQL())[[1]])
+
 
 rowstodisplay <- 100
 
 # Only import values
-loadvldcomextmonhtly <- function(con, productcode_, flowcode_ = 1){
+loadvldcomextmonhtly <- function(con, productcode_, 
+                                 periodmin, periodmax,
+                                 flowcode_ = 1){
     tbl(con, "vld_comext_monthly") %>% 
         filter(productcode %like%  productcode_ & 
                    flowcode == flowcode_ & 
-                   period >= 201701) %>% 
-        collect()
+                   period >= periodmin & period<=periodmax) %>% 
+        addproreppar2tbl(con, .) %>% 
+        collect() %>% 
+        # Remove quotation marks from the product description
+        mutate(productdescription = gsub('"',' ', productdescription)) %>% 
+        select(productdescription, reporter, partner, period, 
+               tradevalue, weight, quantity, quantityraw,
+               productcode, everything())
 }
-swd <- loadvldcomextmonhtly(con, "4407%")    
-plywood <- loadvldcomextmonhtly(con, "4412%")    
-furniturekitchen <- loadvldcomextmonhtly(con, "940340%")
-rm(con) # Where should the database connection object be deleted?
+# swd <- loadvldcomextmonhtly(con, "4407%")
+# plywood <- loadvldcomextmonhtly(con, "4412%")
+# furniturekitchen <- loadvldcomextmonhtly(con, "940340%")
 
-function(input, output) {
+function(input, output, session) {
+    # Load a large data set from the database, based on date and productcode
     datasetInput <- reactive({
-        # Fetch the appropriate data object, depending on the value
-        # of input$dataset.
-        switch(input$productcode,
-               "Sawnwood" = head(swd, rowstodisplay),
-               "Plywood" = head(plywood, rowstodisplay),
-               "Kitchen Furniture" = head(furniturekitchen, rowstodisplay))
+        # Fetch the appropriate data, depending on the value of input$produtcode
+        inputproductcode <- switch(input$productcode,
+               "Sawnwood" = "4407%",
+               "Plywood" = "4412%",
+               "Kitchen Furniture" = "940340%")
+        # Load data from the database
+        con <- RMySQL::dbConnect(RMySQL::MySQL(), dbname = "tradeflows")
+        on.exit(RMySQL::dbDisconnect(con))
+        loadvldcomextmonhtly(con, inputproductcode, 
+                             periodmin = format(input$range[1], "%Y%m"),
+                             periodmax = format(input$range[2], "%Y%m"))
     })
     
-    output$table <- renderTable({
-        datasetInput()
+    
+    # Filter the database based on other variables country etc...
+    datasetfiltered <- reactive({
+        datasetInput() %>% 
+            filter(reporter %in% input$reporter)
     })
+        
+    output$rowsinputtable <- renderText({ 
+        paste(nrow(datasetInput()), "rows")
+    })
+    output$rowsfilteredtable <- renderText({ 
+        paste(nrow(datasetfiltered()), "rows")
+    })
+    
+    # Update list of reporting countries based on the data
+    observe({
+        reporterx <- unique(datasetInput()$reporter)
+        updateSelectizeInput(session, "reporter",
+                             label = "Choose reporting countries:",
+                             choices = reporterx,
+                             selected = reporterx)
+    })
+    
+    output$table <- DT::renderDataTable(datasetfiltered(),
+                                        # Columnd rendering option 
+                                        # Copied from https://rstudio.github.io/DT/options.html
+                                        options = list(columnDefs = list(list(
+                                            targets = 1,
+                                            render = DT::JS(
+                                                "function(data, type, row, meta) {",
+                                                "return type === 'display' && data.length > 15 ?",
+                                                "'<span title=\"' + data + '\">' + data.substr(0, 15) + '...</span>' : data;",
+                                                "}")
+                                        ))), callback = DT::JS('table.page(3).draw(false);'))
     
     # downloadHandler() takes two arguments, both functions.
     # The content function is passed a filename as an argument, and
@@ -48,7 +95,10 @@ function(input, output) {
             sep <- switch(input$filetype, "csv" = ",", "tsv" = "\t")
             
             # Write to a file specified by the 'file' argument
-            write.table(datasetInput(), file, sep = sep,
+            write.table(select(datasetfiltered(),
+                               productcode, everything(), -productdescription,  
+                               productdescription), 
+                        file, sep = sep,
                         row.names = FALSE)
         }
     )
--- docs/visualization/shinycsv/ui.R
+++ docs/visualization/shinycsv/ui.R
@@ -1,17 +1,63 @@
 
+
 fluidPage(
-    titlePanel('File download'),
+    titlePanel("File download from the STIX database", windowTitle = "STIX-Global / eutradeflows"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("productcode", "Choose a product code:", 
+            helpText("Change product or date range to fetch data from the database.",
+                     "Number of rows loaded from the database:"),
+            textOutput("rowsinputtable"),
+            
+            # Input: Specification of the product
+            selectInput("productcode", "Choose a product group:", 
                         choices = c("Sawnwood", "Plywood", "Kitchen Furniture")),
+            
+            # Input: Specification of the date range
+            sliderInput("range", "Date range:",
+                        min = as.Date('2000-01-01'), max = as.Date('2017-09-01'),
+                        value = c(as.Date('2017-01-01'),as.Date('2017-09-01')),
+                        timeFormat = "%Y%m"),
+            helpText("Change country to filter the table in your browser:"),
+            helpText("Number of rows filtered in your browser:"),
+            textOutput("rowsfilteredtable"),
+            
+            # Input: Specification of the reporter country
+            selectizeInput("reporter", "Choose reporting countries:", 
+                           choices = NULL, multiple=TRUE),
             radioButtons("filetype", "File type:",
                          choices = c("csv", "tsv")),
-            downloadButton('downloadData', 'Download')
+            downloadButton('downloadData', 'Download'),
+            width = 3
         ),
         mainPanel(
-            tableOutput('table')
+            DT::dataTableOutput('table')
         )
     )
 )
+
+# 
+# if(FALSE){
+#     # Minimal example tying to solve title page modification issue
+#     # displays <div id="selected_var" in the title
+#     # [Display reactive output tutorial](https://shiny.rstudio.com/tutorial/written-tutorial/lesson4/)
+#     #
+#     # Related question
+#     # https://stackoverflow.com/questions/30876044/shiny-output-not-enclosed-in-tags-is-it-possible
+#     shinyApp(ui = fluidPage(titlePanel(textOutput("selected_var")),
+#                             sidebarLayout(
+#                                 sidebarPanel(
+#                                     selectInput("fruit", "Choose a fruit:", 
+#                                                 choices = c("Apple", "Pear",
+#                                                             "Banana"))),
+#                                 mainPanel(
+#                                     
+#                                     textOutput("selected_var")
+#                                 ))),
+#              server = function(input, output) {
+#                  output$selected_var <- renderText({ 
+#                      paste("The", input$fruit, "page")
+#                  })
+#              }
+#     )
+# }
 
