3eb90cadce08cc3aed6e5d5c9dc45b527718949f
Paul Rougieux <paul.rougieux@gmail.com>
2018-04-19 15:35:47 +0200

    update stix product classification

--- data-raw/product_classifications.R
+++ data-raw/product_classifications.R
@@ -1,10 +1,19 @@
 library(dplyr)
 classificationimm <- read.csv("data-raw/classificationimm.csv", as.is=TRUE)
+# Updated product classifiction received on April 11, 2018
+classificationstix <- read.csv("data-raw/productclassificationstix.csv", as.is = TRUE)
+
 
 classificationimm <- classificationimm %>% 
     select(productcode = TTCN,
            productgroupimm = IMM_major_product_group,
            productimm = IMM_product_summary)
 
-devtools::use_data(classificationimm, overwrite = TRUE)
+classificationstix <- classificationstix %>% 
+    select(productcode = Product_code,
+           productgroupstix = STIX_major_product_group,
+           productstix = STIX_product_summary,
+           productstixorder = STIX_order)
 
+devtools::use_data(classificationimm, overwrite = TRUE)
+devtools::use_data(classificationstix, overwrite = TRUE)
