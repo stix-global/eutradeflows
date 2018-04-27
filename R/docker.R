d0ff3dd720cca27d0c1c1073d35b3d5897035332
Paul Rougieux <paul.rougieux@gmail.com>
2018-04-27 13:08:11 +0200

    update DB connection method for docker

--- R/docker.R
+++ R/docker.R
@@ -5,7 +5,7 @@
 #' between inside the docker container
 #' Connection parameters are passed as environment variables to 
 #' the docker container. Use these to create a RMySQL connection object.
-#' Note on password files :
+#' Note on password files 
 #' Such files are only available containers started in swarm mode (docker stack deploy)
 #' Use the environment variable envpassword = "HARVESTER_DB_PASSWORD_FILE" to pass a file
 #' otherwise use HARVESTER_DB_PASSWORD to pass the password directly.
@@ -13,16 +13,16 @@
 #' @param envuser name of the environment variable that contains the user name
 #' @param envpassword name of the environment variable that contains the password
 #' @return a RMySQL connection object.
-# HARVESTER_DB_HOST: tradeflowsdb:3306
-# HARVESTER_DB_USER: R
-# HARVESTER_DB_PASSWORD_FILE: /run/secrets/db_password
+#' HARVESTER_DB_HOST: tradeflowsdb:3306
+#' HARVESTER_DB_USER: R
+#' HARVESTER_DB_PASSWORD_FILE: /run/secrets/db_password
 #' @export
 dbconnectdocker <- function(dbname = "tradeflows",
                             envuser = "HARVESTER_DB_USER",
                             envpassword = "HARVESTER_DB_PASSWORD",
                             envhost = "HARVESTER_DB_HOST", 
                             envport = "HARVESTER_DB_PORT"){
-    #' If the password is contained in a secret file, read that file
+   # If the password is contained in a secret file, read that file
    if(envpassword == "HARVESTER_DB_PASSWORD_FILE"){
        password <- readLines(Sys.getenv("HARVESTER_DB_PASSWORD_FILE"))
    } else {
@@ -36,4 +36,3 @@ dbconnectdocker <- function(dbname = "tradeflows",
                              port = as.integer(Sys.getenv(envport)))
     return(con)
 }
-
