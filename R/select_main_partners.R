9f01082c18646b07fb3796b9a32344ccaebb77cc
Paul Rougieux <paul.rougieux@gmail.com>
2020-09-07 17:38:51 +0200

    Make check of full series optional in the selection of large country pairs

--- R/select_main_partners.R
+++ R/select_main_partners.R
@@ -50,6 +50,7 @@ select_main_partners <- function(df, slice_n=1){
 #' Select main tropical partners for the product of interest and related products
 #' @param wood_imports data frame of wood imports from comext
 #' @param vpa_partners data frame containing the country code of VPA partners
+#' @param force_full_series boolean to force selection of only country pairs which have all data points for all period
 #' @examples
 #' # Load the main partners of tropical sawnwood
 #' swd_trop_main <- select_main_partners(wood_imports, vpa_partners, "44072969")
@@ -57,7 +58,10 @@ select_main_partners <- function(df, slice_n=1){
 #' ply_trop_main <- select_main_partners(wood_imports, vpa_partners, "44123190")
 #' @return data fram of main tropical sawnwood partners
 #' @export
-select_main_partners_trop <- function(wood_imports, vpa_partners, product_code_of_interest){
+select_main_partners_trop <- function(wood_imports, 
+                                      vpa_partners, 
+                                      product_code_of_interest, 
+                                      force_full_series=TRUE){
     df <- select_wood_imports(wood_imports=wood_imports,
                               vpa_partners=vpa_partners,
                               product_code_of_interest=product_code_of_interest)
@@ -73,14 +77,19 @@ select_main_partners_trop <- function(wood_imports, vpa_partners, product_code_o
     # Error if n is greater than len(unique(df$period))
     stopifnot(max(large_country_pairs$n)<=length(unique(df$period)))
 
-    # See the output of this table in sawnwood_tropical.Rmd
-    large_country_pairs_full_series <- large_country_pairs %>%
-        filter(n==max(n) & !is.na(weight)) %>%
+    # Optionally remove country pairs with missing data
+    if(force_full_series){
+        # See the output of this table in sawnwood_tropical.Rmd
+        large_country_pairs <- large_country_pairs %>%
+            filter(n==max(n) & !is.na(weight))
+    }
+    # Keep only the reporter and partner columns for the right join
+    large_country_pairs <- large_country_pairs %>%
         select(reporter, partner)
 
+    # Right join to the main input data to keep only the country pairs of interest
     df_main <- df %>%
-        # Use a right join to select only the country pairs of interest
-        right_join(large_country_pairs_full_series, by = c('reporter', 'partner'))
+        right_join(large_country_pairs, by = c('reporter', 'partner'))
     return(df_main)
 }
 
